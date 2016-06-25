6f92f078b9d190d2fcb85a64e0036ff21e62b43f
Javier G. Sogo <jgsogo@gmail.com>
2016-06-25 21:39:31 +0200

    thinking on typed grouped queryset

--- queryset/models/queryset_typed.h
+++ queryset/models/queryset_typed.h
@@ -8,6 +8,24 @@
 namespace qs {
     
     template <typename Type, typename... Args>
+    class TypedQuerySet;
+
+    template <typename Type, typename T, typename... Args>
+    class GroupedTypedQuerySet : public GroupedQuerySet<T, Args...> {
+        using BaseQs = GroupedQuerySet<T, Args...>;
+    public:
+        GroupedTypedQuerySet(const utils::ImplDataSource<Args...>& datasource) : BaseQs(datasource) {}
+        //GroupedQuerySet(const BaseQs& other) : BaseQs(other), _evaluated(false) {}
+        GroupedTypedQuerySet(const BaseQuerySet<Args...>& other) : BaseQs(other) {}
+        GroupedTypedQuerySet(const GroupedQuerySet<T, Args...>& other) : BaseQs(other) {}
+
+        typename const TypedQuerySet<Type, Args...>& at(const T& t) const {
+            return this->eval().at(t);
+        }
+    };
+
+
+    template <typename Type, typename... Args>
     class TypedQuerySet : public QuerySet<Args...> {
         using BaseQs = QuerySet<Args...>;
         public:
@@ -17,6 +35,14 @@ namespace qs {
             const Type get(const typename std::tuple_element<0, std::tuple<Args...>>::type& t) {
                 return Type(BaseQs::get(t));
             }
+
+            // Grouping by field types
+            template <typename T>
+            GroupedTypedQuerySet<Type, T, Args...> groupBy() {
+                assert(!BaseQs::is_evaluated());
+                return GroupedTypedQuerySet<Type, T, Args...>(*this);
+            }
     };
+
 
 }
