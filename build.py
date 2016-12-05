f856a94809d71b7e46d9dd0bca83d60d8b290191
Javier G. Sogo <jgsogo@gmail.com>
2016-12-05 13:36:07 +0100

    limit versions of compilers

--- build.py
+++ build.py
@@ -6,6 +6,10 @@ username = os.getenv("CONAN_USERNAME", "jgsogo")
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username=username)
+    builder = ConanMultiPackager(username=username, 
+                                 visual_versions=["14"],
+                                 gcc_versions = ["5.2", "5.3", "5.4", "6.2"],
+                                 apple_clang_versions = ["5.0", "5.1", "6.0", "6.1", "7.0", "7.3", "8.0"]
+                                )
     builder.add_common_builds()
     builder.run()
