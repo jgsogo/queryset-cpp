4bdc485413359a003a5cf63a96285d503a5f0ceb
Javier G. Sogo <jgsogo@gmail.com>
2016-12-21 16:59:47 +0100

    pure_c = False

--- build.py
+++ build.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
                                  apple_clang_versions = ["6.1", "7.0", "7.3", "8.0"],
                                  args = "--scope queryset-cpp:BUILD_TEST=1 --build=missing"
                                 )
-    builder.add_common_builds()
+    builder.add_common_builds(pure_c=False)
     print("{} builds ahead!".format(len(builder.builds)))
     builder.run()
