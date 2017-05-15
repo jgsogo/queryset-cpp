c1ee0428f63c47848c710a8d762232aecdf2ddbf
Javier G. Sogo <jgsogo@gmail.com>
2017-05-15 22:48:50 +0200

    less compiler versions, but enough

--- build.py
+++ build.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
     builder = ConanMultiPackager(username=username,
                                  reference=reference,
                                  visual_versions=["14",],
-                                 gcc_versions = ["5.2", "5.3", "5.4", "6.2",],
-                                 apple_clang_versions = ["6.1", "7.0", "7.3", "8.0",],
+                                 gcc_versions = ["5.4", "6.3",],
+                                 apple_clang_versions = ["7.3", "8.1",],
                                  args = "--scope queryset-cpp:BUILD_TEST=1 --build=missing"
                                 )
     builder.add_common_builds(shared_option_name="queryset-cpp:shared")
@@ -24,3 +24,4 @@ if __name__ == "__main__":
     builder.builds = filtered_builds
     print("{} builds ahead!".format(len(builder.builds)))
     builder.run()
+
