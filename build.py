19013d43ab3fce18072b6a3b48b252a4faac9923
Javier G. Sogo <jgsogo@gmail.com>
2017-08-30 18:49:25 +0200

    refactor travis

--- build.py
+++ build.py
@@ -11,15 +11,15 @@ reference = os.getenv("CONAN_REFERENCE", "{}/{}".format(QuerysetCPP.name, Querys
 if __name__ == "__main__":
     builder = ConanMultiPackager(username=username,
                                  reference=reference,
-                                 visual_versions=["14",],
                                  args = "--scope queryset-cpp:BUILD_TEST=1 --build=missing"
                                 )
     builder.add_common_builds(shared_option_name="queryset-cpp:shared")
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
-        if settings["build_type"] == "Release":
+        if settings["arch"] == "x86_64" and settings["build_type"] == "Release":
             filtered_builds.append([settings, options, env_vars, build_requires])
     builder.builds = filtered_builds
+
     print("{} builds ahead!".format(len(builder.builds)))
     builder.run()
 
